eb357df06558d373505a389c4319b1510491904a
maisdwekat <162214298+maisdwekat@users.noreply.github.com>
2024-04-07 01:27:31 +0200

    Update DeliveryOrPickup.java

--- src/main/java/orderFood/DeliveryOrPickup.java
+++ src/main/java/orderFood/DeliveryOrPickup.java
@@ -1,47 +1,43 @@
 package orderFood;
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 
 /**
  * The DeliveryOrPickup class represents the delivery or pickup options for a food order.
  * It allows users to select their preferred delivery address or pickup location and time.
  */
 public class DeliveryOrPickup {
-
     private String deliveryAddress;
     private String pickupLocation;
     private String pickupTime;
+
+    private static final Logger logger = Logger.getLogger(DeliveryOrPickup.class.getName());
 
     /**
      * Selects the delivery option for the food order and sets the delivery address.
      *
      * @param address The delivery address.
      */
-   public void selectDeliveryOption(String address) {
-    setDeliveryAddress(address);
-    Logger logger = Logger.getLogger(getClass().getName());
-    final String ADDRESS_CONFIRMATION_MESSAGE = "Delivery address confirmed: %s";
-    final String estimatedDeliveryTime = "Estimated delivery time: 2 hours";
-    logger.info(String.format(ADDRESS_CONFIRMATION_MESSAGE, address));
-    logger.info(estimatedDeliveryTime);
-}
+    public void selectDeliveryOption(String address) {
+        setDeliveryAddress(address);
+        final String ADDRESS_CONFIRMATION_MESSAGE = "Delivery address confirmed: %s";
+        final String estimatedDeliveryTime = "Estimated delivery time: 2 hours";
+        logger.info(String.format(ADDRESS_CONFIRMATION_MESSAGE, address));
+        logger.info(estimatedDeliveryTime);
+    }
 
-    private boolean shouldLogDeliveryTime() {
-    
-    return true; 
-}
     /**
      * Selects the pickup option for the food order and sets the pickup location and time.
      *
      * @param location The pickup location.
      * @param time     The preferred pickup time.
      */
-     public void selectPickupOption(String location, String time) {
+    public void selectPickupOption(String location, String time) {
         setPickupLocation(location);
         setPickupTime(time);
-        System.out.println("Pickup location confirmed: " + location);
-        System.out.println("Available pickup time slots: 10:00 AM, 12:00 PM, 2:00 PM");
-        System.out.println("Preferred pickup time selected: " + time);
+        logger.info("Pickup location confirmed: " + location);
+        logger.info("Available pickup time slots: 10:00 AM, 12:00 PM, 2:00 PM");
+        logger.info("Preferred pickup time selected: " + time);
     }
 
     /**
